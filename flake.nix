32c4c86ac9df923a42a2e3e60226ad82e94b17e6
hugosenari <hugosenari@gmail.com>
2022-03-12 01:37:50 -0300

    chore: re index zws

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = ''A command line interface for shortening URLs with ZWS instances'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."zws-main".url = "path:./main";
+  inputs."zws-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_0_0".url = "path:./v1_0_0";
+  inputs."zws-v1_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_0_1".url = "path:./v1_0_1";
+  inputs."zws-v1_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_1_0".url = "path:./v1_1_0";
+  inputs."zws-v1_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_1_1".url = "path:./v1_1_1";
+  inputs."zws-v1_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_1_2".url = "path:./v1_1_2";
+  inputs."zws-v1_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_1_3".url = "path:./v1_1_3";
+  inputs."zws-v1_1_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_1_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_2_0".url = "path:./v1_2_0";
+  inputs."zws-v1_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_2_1".url = "path:./v1_2_1";
+  inputs."zws-v1_2_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_2_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_2_2".url = "path:./v1_2_2";
+  inputs."zws-v1_2_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_2_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_3_0".url = "path:./v1_3_0";
+  inputs."zws-v1_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_4_0".url = "path:./v1_4_0";
+  inputs."zws-v1_4_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_4_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zws-v1_5_0".url = "path:./v1_5_0";
+  inputs."zws-v1_5_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zws-v1_5_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v1_2_2/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A command line interface for shortening URLs with ZWS instances'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-zws-v1_2_2.flake = false;
+  inputs.src-zws-v1_2_2.owner = "zws-im";
+  inputs.src-zws-v1_2_2.ref   = "refs/tags/v1.2.2";
+  inputs.src-zws-v1_2_2.repo  = "cli";
+  inputs.src-zws-v1_2_2.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-zws-v1_2_2"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-zws-v1_2_2";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
